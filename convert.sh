9dd0e7f0b5efc8a7e481687e02a1059406dad794
Ben Talberg <ben.talberg@gmail.com>
2020-08-19 17:31:05 -0500

    add flexibility in setting class names

--- convert.sh
+++ convert.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-if [ "x$1" = "x" ]; then
-    echo "Usage: ./convert.sh <<wf_coco_data_url>>"
-    exit 0
-fi
-
 url=$1
-filename_with_extension="${url##*/}"
-filename_naked="${filename_with_extension%.tar.gz}"
 
-if [ ! -d convert2Yolo ]; then
-	git clone https://github.com/WildflowerSchools/convert2Yolo
-    pip install -r ./convert2Yolo/requirements.txt
-fi
+if [ "x${url}" != 'x' ]; then
+    filename_with_extension="${url##*/}"
+    filename_naked="${filename_with_extension%.tar.gz}"
 
-if [ ! -f "./${filename_with_extension}" ]; then
-	wget $url
+    if [ ! -f "./${filename_with_extension}" ]; then
+        wget $url
 
-	if [ $? -ne 0 ]; then
-        echo "Could not download ${url}"
-        exit 0
+        if [ $? -ne 0 ]; then
+            echo "Could not download ${url}"
+            exit 0
+        fi
     fi
-fi
 
-if [ ! -d data ]; then
-	rm -rf ./data
+    if [ -d data ]; then
+        rm -rf ./data
+    fi
 
-	if [ ! -d "./${filename_naked}" ]; then
+    if [ ! -d "./${filename_naked}" ]; then
         tar -xvf "${filename_with_extension}"
     fi
 
     mv "${filename_naked}" data
+fi
+
+if [ ! -d data ]; then
+    echo "Error: Data folder missing, either create or specify a url to download data"
+    exit 0
+fi
+
+if [ ! -d convert2Yolo ]; then
+	git clone https://github.com/WildflowerSchools/convert2Yolo
+    pip install -r ./convert2Yolo/requirements.txt
 fi
 
 if [ ! -f ./coco.names ]; then
@@ -46,6 +49,11 @@ mkdir -p output/wf
 
 cp ./coco.names ./output/wf/.
 cp ./wf_yolo.data ./output/wf/.
+
+class_count=$(sed '/^\s*$/d' ./coco.names | wc -l)
+class_count="$(echo -e "${class_count}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+sed -i'.bak' -E "s/classes=[[:digit:]]+/classes=${class_count}/g" ./output/wf/wf_yolo.data  && rm ./output/wf/wf_yolo.data.bak
 
 echo "Copying Images to YOLO labels folder"
 cp -R ./data/images/ ./output/wf/labels
